d27d775c84a8b8022fd7d45dd8c9b9a1ed0cfc8c
jennifer-s19 <siaojm@yahoo.com>
2020-02-01 14:37:50 -0800

    adding drop down menu, getting rid of logos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import ReactDOM from 'react';
+import Paper from '@material-ui/core/Paper'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+// import TodoList from './TodoList'
+// import TodoEntryForm from './TodoEntryForm';
 
-function App() {
+
+function PackageList(props) {
+  const packages = props.packages;
+  const listItems = packages.map((packages) => 
+    <option>
+    {packages}
+    </option> 
+    );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+    <Container style={{marginTop: 20}}>
+      <Paper style={{padding: 20, backgroundColor: '#fffff3', margin:0, }}>
+        <Typography variant="h3" align="center">time dream</Typography>
+        
+        {/* <TodoList todos={todos} deleter={deleteTodo}/> */}
+        {/* <TodoEntryForm adder={addTodo} /> */}
+      </Paper>
+    </Container>
+    <Typography variant="h6" align="left" > <select>{listItems}</select></Typography>
     </div>
-  );
+    );
 }
 
-export default App;
+
+export default function App() {
+  const packages = ["node.js", "npm"];
+  return (
+    <PackageList packages={packages} />
+    );
+}
