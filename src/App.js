c03b6436cd47cb66f67ea82355920da3492c83c3
kirakira0 <kiratoal0.com>
2020-02-01 14:09:29 -0800

    You can now add data points to the graph

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* App.js */
 //import 
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -16,9 +16,24 @@ var Component = React.Component;
 //var CanvasJSReact = require('canvasjs.react');
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-class App extends Component {
-	render() {
-		const options = {
+function App() {
+  
+    const[content, setContent] = useState(""); 
+  
+    const[dataPoints, setDataPoints] = useState(
+      [
+
+        { x: 0, y: 4 },
+        { x: 5, y: 10 },
+        { x: 10, y: 10 },
+        { x: 15, y: 28 },
+        { x: 20, y: 4 },
+        { x: 25, y: 30 },
+        { x: 30, y: 2 },
+      ]
+    ); 
+
+    const options = {
 			animationEnabled: true,
 			title:{
 				text: "node.js"
@@ -37,14 +52,7 @@ class App extends Component {
         // xValueFormatString: "MMMM",
         xValueFormatString: "#",
 				type: "spline",
-				dataPoints: [
-          { x: 5, y: 10 },
-          { x: 10, y: 10 },
-					{ x: 15, y: 28 },
-					{ x: 20, y: 4 },
-					{ x: 25, y: 30 },
-					{ x: 30, y: 2 },
-				]
+				dataPoints: dataPoints
 			}]
     }
     
@@ -52,22 +60,42 @@ class App extends Component {
       step: 5,
     };
 
+    function submit(e) {
+      e.preventDefault(); 
+      const x = +content;
+      if (0 > x || x > 30) {
+        return; 
+      }
+      console.log(content); 
+      const i = dataPoints.findIndex(p => p.x === x); 
+      console.log(i); 
+
+      if (i == -1) {
+        return;
+      }
+      dataPoints[i].y += 1;
+      const y = dataPoints[i].y; 
+      setDataPoints([...dataPoints]);
+    }; 
+
 		return (
 		<div>
 			<CanvasJSChart options = {options}
 				/* onRef={ref => this.chart = ref} */
 			/>
 			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-      <p>It took me approximatley </p>
-      <TextField id="number" type="number" inputProps={inputProps} />
-      <p> minutes to install this package. </p>
-      <Button variant="contained" color="primary"> Add Data Point </Button>
+
       
+      <form onSubmit = {submit}>
+        <p>It took me approximatley </p>
+        <TextField id="time" type="number" inputProps={inputProps} onChange={e => setContent(e.target.value)} />
+        <p> minutes to install this package. </p>
+        <Button type= "submit" variant="contained" color="primary"> Add Data Point </Button>
+      </form>
       
 
     </div>
 		);
-  }
 }
 
 
